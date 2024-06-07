f0a441638dc9cd4c5cad1fbcd2fd6be888ff9657
VincentMiras <130740022+VincentMiras@users.noreply.github.com>
2024-06-07 11:39:02 -0400

    Update AISDataCleanerV1.py
    clean misplaced points from tracks

--- AISDataCleanerV1.py
+++ AISDataCleanerV1.py
@@ -1,17 +1,11 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-"""
-
-
 import pandas as pd
 from zipfile import ZipFile
 import os
-
+import numpy as np
 
 cols_to_read = ['MMSI', 'Time', 'Longitude', 'Latitude', 'COG', 'SOG']
 directory='D:\Vincent\SPAR_AIS_DATA'
-save_directory='D:\Vincent\Cleandata'
+save_directory='D:\Vincent\Cleandata_nbp_mindist_mieux'
 
 def process_csv_file(csv_files):
     df = pd.read_csv(csv_files, usecols=cols_to_read)
@@ -22,10 +16,40 @@ def process_csv_file(csv_files):
     df = df.dropna(subset=['Longitude'])
 
     df=df[(df['Latitude'] >= 30) & (df['Latitude'] <= 89.999)]
-    df=df[(df['Longitude'] >= -179.999) & (df['Latitude'] <= 179.999)]
-
-    df.sort_values(by=['MMSI','Time'], inplace=True)
-    return df
+    df=df[(df['Longitude'] >= -179.999) & (df['Longitude'] <= 179.999)]
+    
+    df['Time']=pd.to_datetime(df['Time'], format='%Y%m%d_%H%M%S')
+    
+    
+    mmsi_counts = df['MMSI'].value_counts()
+    mmsi_to_keep = mmsi_counts[mmsi_counts >= 10].index
+    df_filtered = df[df['MMSI'].isin(mmsi_to_keep)]
+    
+    to_remove_loin = []
+    for mmsi in df_filtered['MMSI'].unique():
+        mmsi_data = df_filtered[df_filtered['MMSI'] == mmsi]
+        distances = np.sqrt(((mmsi_data[['Latitude', 'Longitude']].values[:, np.newaxis] - mmsi_data[['Latitude', 'Longitude']].values) ** 2).sum(axis=2))
+    
+    # Exclure la distance avec le point lui-même en remplissant la diagonale avec une valeur infinie
+        np.fill_diagonal(distances, np.inf)
+        
+        min_distances = np.min(distances, axis=1)
+        indices_to_remove_loin = np.where(min_distances > 0.5)[0]
+        to_remove_loin.extend(mmsi_data.index[indices_to_remove_loin])
+    
+    df_filtered = df_filtered.drop(to_remove_loin)
+    
+    to_remove = []
+    for mmsi in df_filtered['MMSI'].unique():
+        mmsi_data = df_filtered[df_filtered['MMSI'] == mmsi]
+        if mmsi_data['Latitude'].max() - mmsi_data['Latitude'].min() < 0.1 and mmsi_data['Longitude'].max() - mmsi_data['Longitude'].min() < 0.1:
+            to_remove.extend(mmsi_data.index)
+    
+    df_filtered = df_filtered.drop(to_remove)
+    
+    df_filtered.sort_values(by=['MMSI','Time'], inplace=True)
+    
+    return df_filtered
     
 def process_files_in_directory(directory):
     for year in os.listdir(directory):
@@ -41,7 +65,7 @@ def process_files_in_directory(directory):
                 continue
             for day in os.listdir(file_month):
                 file_day = os.path.join(file_month, day)
-                if file_day.endswith('.zip'):
+                if file_day.endswith('.zip'): 
                     with ZipFile(file_day, 'r') as zip_ref:
                         temp_dir = r'D:\Vincent\SPAR_AIS_DATA\temp'
                         zip_ref.extractall(temp_dir)
@@ -54,6 +78,43 @@ def process_files_in_directory(directory):
                             os.remove(extracted_file_path)
                     os.rmdir(temp_dir)
                 
-                
-                
-            
+csv_test='exactEarth_historical_data_2023-01-01.csv'
+
+def ecart_mesure(csv_test):
+    df = pd.read_csv(csv_test, usecols=cols_to_read)
+    df['Time']=pd.to_datetime(df['Time'], format='%Y%m%d_%H%M%S')
+    df.sort_values(by=['MMSI','Time'], inplace=True)
+    df['Time_diff'] = df.groupby('MMSI')['Time'].diff().dt.total_seconds()
+    
+    mmsi_counts = df['MMSI'].value_counts()
+    mmsi_to_keep = mmsi_counts[mmsi_counts >= 10].index
+    df_filtered = df[df['MMSI'].isin(mmsi_to_keep)]
+    
+    to_remove_loin = []
+    for mmsi in df_filtered['MMSI'].unique():
+        mmsi_data = df_filtered[df_filtered['MMSI'] == mmsi]
+        distances = np.sqrt(((mmsi_data[['Latitude', 'Longitude']].values[:, np.newaxis] - mmsi_data[['Latitude', 'Longitude']].values) ** 2).sum(axis=2))
+    
+    # Exclure la distance avec le point lui-même en remplissant la diagonale avec une valeur infinie
+        np.fill_diagonal(distances, np.inf)
+        
+        min_distances = np.min(distances, axis=1)
+        indices_to_remove_loin = np.where(min_distances > 1)[0]
+        to_remove_loin.extend(mmsi_data.index[indices_to_remove_loin])
+    
+    df_filtered = df_filtered.drop(to_remove_loin)
+    
+    to_remove = []
+    for mmsi in df_filtered['MMSI'].unique():
+        mmsi_data = df_filtered[df_filtered['MMSI'] == mmsi]
+        if mmsi_data['Latitude'].max() - mmsi_data['Latitude'].min() < 0.1 and mmsi_data['Longitude'].max() - mmsi_data['Longitude'].min() < 0.1:
+            to_remove.extend(mmsi_data.index)
+    
+    df_filtered = df_filtered.drop(to_remove)
+    
+    # Calcul de la moyenne et de l'écart-type pour chaque MMSI
+    result = df_filtered.groupby('MMSI')['Time_diff'].agg(['mean', 'std','count']).reset_index()
+    result.sort_values(by=['mean'], inplace=True)
+    df_filtered.to_csv('testclean.csv', index=False)
+    return(df_filtered)
+   
